6c4f56d4fb86645b0ae0328f49370d52b09c7f04
artoD2 <artosekepyan@gmail.com>
2020-10-30 10:42:45 -0600

    My Heroku

---
+++ public/precache-manifest.9430377190a09912afbc78f0ba84676d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7b592cf7c341be9ad3765cd7e86a226",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9aeb764252632e1222f6",
+    "url": "/static/css/main.d37a3604.chunk.css"
+  },
+  {
+    "revision": "adbadc3ed2fd3cdcfe5e",
+    "url": "/static/js/2.66c9cc2d.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.66c9cc2d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9aeb764252632e1222f6",
+    "url": "/static/js/main.86c653f1.chunk.js"
+  },
+  {
+    "revision": "c1d1cf000042f319a405",
+    "url": "/static/js/runtime-main.aa7b8702.js"
+  }
+]);
